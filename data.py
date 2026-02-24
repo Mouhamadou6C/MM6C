2558000ae47c3f89cce9dfaf46ccb60492b7b187
Mouhamadou6C <mouhamadouc47@gmail.com>
2026-02-24 22:49:07 +0000

    Add files via upload

---
+++ data.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title ( "Application de Collecte")
+
+st.header("Données collectées")
+st.number_input(" Entre le coordonné de X : ")
+st.number_input(" Entre le coordonné de y : ")
+st.number_input(" Donner le z : ")
